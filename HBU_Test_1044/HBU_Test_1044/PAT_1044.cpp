88937872da3cf82d298f650788514ec7a5a1a561
T-Rex <1296821114@qq.com>
2018-02-05 20:26:10 +0800

    PAT_乙级_1044

---
+++ HBU_Test_1044/HBU_Test_1044/PAT_1044.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+
+std::string low_letter[13]  = { "tret","jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
+std::string high_letter[13] = { "tret","tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou" };
+
+void read(std::string tmp)
+{
+	if (tmp[0] >= '0'&&tmp[0] <= '9')
+	{
+		int n = 0;
+		for (int i = 0; i < tmp.size(); i++)
+			n = n * 10 + tmp[i] - '0';
+		if (n < 13)
+			std::cout << low_letter[n] << std::endl;
+		else
+		{
+			std::cout << high_letter[n / 13] << " " << low_letter[n % 13] << std::endl;
+		}
+	}
+	else
+	{
+		int low=0, high=0;
+		if (tmp.size() > 5)
+		{			
+			std::string s1, s2;
+			s1= tmp.substr(0, 3);
+			if (tmp.size() == 9)
+				 s2 = tmp.substr(4, 9);
+			else 
+				 s2 = tmp.substr(4, 8);
+			for (int i = 0; i < 13; i++)
+			{
+				if (s2 == low_letter[i])
+				{
+					low = i;
+				}
+				if (s1 == high_letter[i])
+				{
+					high = i;
+				}
+			}
+		}
+		else
+		{
+			high = 0;
+			for (int i = 0; i < 13; i++)
+			{
+				if (tmp==low_letter[i])
+				{
+					low = i;
+				}
+				if (tmp == high_letter[i])
+				{
+					low = i*13;
+				}
+			}
+		}
+		std::cout << low + high * 13 << std::endl;
+	}
+
+}
+
+int main()
+{
+	int T;
+	std::string tmp;
+	std::cin >> T;
+	getchar();
+	while (T--)
+	{
+		getline(std::cin, tmp);
+		read(tmp);
+
+	}
+	system("pause");
+	return 0;
+}
